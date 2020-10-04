98b392fe5beafa7d4b91c912246368b0641d1a00
Chris Mills <coderchrismills@gmail.com>
2020-10-03 23:08:51 -0700

    Removing AlamoFire, not using it.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.3.0")),
         .package(url: "https://github.com/mxcl/Chalk.git", from: "0.1.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0"),
         .package(url: "https://github.com/JanGorman/Table", from: "1.0.0"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.5.0")
@@ -21,7 +20,6 @@ let package = Package(
         .target(name: "NYTBooks", dependencies: [
             .product(name: "ArgumentParser", package: "swift-argument-parser"),
             .product(name: "Chalk", package: "Chalk"),
-            .product(name: "Alamofire", package: "Alamofire"),
             .product(name: "SwiftyJSON", package: "SwiftyJSON"),
             .product(name: "Table", package: "Table"),
             .product(name: "SwiftyTextTable", package: "SwiftyTextTable")
